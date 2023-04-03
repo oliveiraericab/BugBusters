1184b1502591e49c0f1c03c758d55cc5fb800f25
oliveiraericab <oliveiraericab@gmail.com>
2023-04-03 20:58:36 -0300

    Criei teste CreateToken- função GET/ Falta extrair token

--- src/test/java/apiTest/TestBooking.java
+++ src/test/java/apiTest/TestBooking.java
@@ -11,14 +11,10 @@ import java.nio.file.Paths;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
 
-
-    @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
     public class TestBooking {   //Início da classe
         //Atributos
         static String ct = "application/json";  //content type
-        static String uriUser = "https://restful-booker.herokuapp.com/booking/";
-
-        // static String ac = "application/json"; //accept
+        static String uriUser = "https://restful-booker.herokuapp.com/";
 
         //Funções e métodos
         //Funções de apoio
@@ -26,6 +22,23 @@ import static org.hamcrest.Matchers.*;
             return new String(Files.readAllBytes(Paths.get(arquivoJson)));
         }
         //Funções  de Testes
+
+        @Test
+        public void testarCreateToken() throws IOException {
+            String jsonBody = lerArquivoJson("src/test/resources/json/user2.json");
+
+            given()
+                    .contentType(ct)                             // tipo de conteúdo
+                    .log().all()                                 // mostre tudo
+                    .body(jsonBody)                              // corpo da requisição
+                    .when()
+                    .post(uriUser + "auth")
+                    .then()
+                    .contentType(ct)
+                    .log().all()
+                    .body("token", hasLength(15))
+            ;
+        }
 
         @Test
         public void testarCreateBooking() throws IOException {
@@ -53,13 +66,14 @@ import static org.hamcrest.Matchers.*;
 
         @Test
         public void testarGetBooking(){
-            String id = "4347";
-            /
+            String id = "5973";
+
                 given()
                     .contentType(ct)
                     .log().all()
                 .when()
-                    .get(uriUser + id)
+                    .get("booking/" + uriUser + id)
+
                 .then()
                     .contentType(ct)
                     .log().all()
@@ -72,8 +86,9 @@ import static org.hamcrest.Matchers.*;
 
         }
     }
-        /*
-        @Test @Order(3)
+    /*
+
+        @Test
         public void testarAlterarUser() throws IOException {
             String jsonBody = lerArquivoJson("src/test/resources/json/user2.json");
             String userId = "15215912";
@@ -112,30 +127,6 @@ import static org.hamcrest.Matchers.*;
             ;
         }
 
-        @Test @Order(5)
-        public void testarLogin(){
-            String username = "malagueta";
-            String password = "123456";
-
-            Response resp = (Response) given()
-                    .contentType(ct)
-                    .log().all()
-                    .when()
-                    .get(uriUser + "login?username=" + username + "&password=" + password + "")
-                    .then()
-                    .log().all()
-                    .statusCode (200)
-                    .body("code", is(200))
-                    .body( "type", is("unknown"))
-                    .body("message", containsString("logged in user session:"))
-                    .body("message", hasLength(36))
-                    .extract()
-                    ;
-
-            //Extração do token
-            String token = resp.jsonPath().getString("message").substring(23);
-            System.out.println("Conteúdo do Token " + token);
-        }
         @ParameterizedTest @Order(6)
         @CsvFileSource(resources = "csv/massaUser.csv", numLinesToSkip = 1, delimiter = ',')
         public void testarIncluirUserCSV(
